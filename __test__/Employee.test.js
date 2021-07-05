f4c0f4c67c4a3a1950a234de0853b583ced302ae
Ryan Blessing <ryanblessing20@yahoo.com>
2021-07-05 14:29:53 -0500

    updated testing

---
+++ __test__/Employee.test.js
@@ -0,0 +1,38 @@
+const Employee = require("../lib/Employee");
+
+test('test create new employee', () => {
+    const employee = new Employee();
+
+    expect(typeof(employee)).toBe('object')
+});
+
+test('set employee Id', () => {
+    const id = '1';
+    const employee = new Employee(id, 'Ryan', 'Blessing', 'employee')
+
+    expect(employee.getId()).toBe(id)
+});
+
+test('set first name', () => {
+    const firstName = 'Ryan';
+    const employee = new Employee('1', firstName, 'Blessing', 'employee')
+
+    expect(employee.getName()).toBe(firstName)
+});
+
+test('set last name', () => {
+    const lastName = 'Blessing';
+    const employee = new Employee('1', 'Ryan', lastName, 'employee')
+
+    expect(employee.getName()).toBe(lastName)
+});
+
+
+
+
+test('To set the employees role in company', () => {
+    const role = "Employee"
+    const employee = new Employee('1', 'Ryan', 'Blessing', role)
+
+    expect(employee.getRole()).toBe(role)
+});
